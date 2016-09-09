b0ff4be93b618b2feb68c41cdcc3b2d2c2b080d8
Krishan Arya <krishanmarya1@gmail.com>
2016-09-09 22:23:38 +1000

    Integrate Mongoose

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var mongoose= require('mongoose');
+var Email= require('./models.js');
+mongoose.Promise = require('bluebird');
+//assert.equal(query.exec().constructor, require('bluebird'));
 
 app.use(express.static(__dirname + '/client'));
 app.use(express.static(__dirname + '/client/public'));
@@ -10,26 +13,29 @@ app.use(express.static(__dirname + '/client/source'));
 
 
 var MongoClient = require('mongodb').MongoClient
-
 var URL = 'mongodb://localhost:27017/mydatabase'
+
+
+
+mongoose.connect(URL);
+
 
 io.on('connection', function(socket){
   socket.on('sendEmailAddress', function(msg){
     console.log(msg,socket.id);
 
+    var email = new Email();
+    email.address=msg;
 
-    MongoClient.connect(URL, function(err, db) {
-  if (err) return
+     email.save(function(err) {
+            if (err)
+                console.log(err);
 
-  var emails = db.collection('emails');
+io.emit('chat message', `email added ${msg}`);
+           
+        });
 
-  emails.insert({address: msg}, function(err, result) {
-    emails.find({address: msg}).toArray(function(err, docs) {
-      console.log(docs[0])
-      db.close()
-    })
-  })
-})
+
 
   });
 });
@@ -37,3 +43,17 @@ io.on('connection', function(socket){
 // Cant use app here b/c it interferes w/ sockets.
 http.listen(3000, "127.0.0.1");
 
+
+
+//   MongoClient.connect(URL, function(err, db) {
+//   if (err) return
+
+//   var emails = db.collection('emails');
+
+//   emails.insert({address: msg}, function(err, result) {
+//     emails.find({address: msg}).toArray(function(err, docs) {
+//       console.log(docs[0])
+//       db.close()
+//     })
+//   })
+// })
