5e1f4aed0a5dc6fff67c107aefec0fb1278bb940
Krishan Arya <krishanmarya1@gmail.com>
2016-09-10 22:13:46 +1000

    Add http to tech news

--- client/app.js
+++ client/app.js
@@ -1,30 +1,36 @@
-var myApp = angular.module('myApp', ['ngRoute'])
+var myApp = angular.module('myApp', ['ngRoute', 'ngSanitize'])
 
 
 myApp.controller('myCtrl', function($scope) {
   $scope.repeatables = ["Send something, please!"];
   $scope.check = 'Angular is registered';
+  $scope.stories = [];
+  $scope.error=null;
 
   $scope.sendEmailAddress = function(emailAddress) {
 
     socket.emit('sendEmailAddress', emailAddress);
 
     socket.once('loggedToDB', function(msg) {
+      console.log(msg)
+
 
       $scope.$apply(function() {
-        $scope.repeatables = ["Email has been Added", "congrats!"];
+        $scope.stories=msg;
+        $scope.error="Email has been added, Congrats!"
       })
     })
 
     socket.once('emailExtant', function(msg) {
       $scope.$apply(function() {
-        $scope.repeatables = ["Not Added", "This email is already in our DB"];
+        $scope.error= "Not Added, This email is already in our DB"
+        $scope.stories = [];
       })
     });
 
     socket.once('invalidEmail', function(msg) {
       $scope.$apply(function() {
-        $scope.repeatables = ["Please enter a valid Email Address", "Do it now!"];
+        $scope.error= "Please enter a valid Email Address. Do it now!"
       })
     });
 
@@ -46,3 +52,24 @@ myApp.config(function($routeProvider, $locationProvider) {
 
   $locationProvider.html5Mode(true);
 })
+
+// Directives
+myApp.directive('helloWorld', function() {
+  return {
+    restrict: 'AE',
+    replace: true,
+    template: '<p style="background-color:{{color}}">Hello World</p>',
+    link: function(scope, elem, attrs) {
+      //elem ===p above;
+      elem.bind('click', function() {
+        elem.css('background-color', 'white');
+        scope.$apply(function() {
+          scope.color = "black";
+        });
+      });
+      elem.bind('mouseover', function() {
+        elem.css('cursor', 'pointer');
+      });
+    }
+  };
+});
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var io = require('socket.io')(http);
 var mongoose = require('mongoose');
 var Email = require('./db/models.js');
 var path = require('path');
+var request = require("request");
 var MongoClient = require('mongodb').MongoClient
 mongoose.Promise = require('bluebird');
 
@@ -21,40 +22,54 @@ app.use(express.static(__dirname + '/client/public'));
 app.use(express.static(__dirname + '/client/source'));
 
 // Regex checker
-var reg= /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
+var reg = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
 
 //sockets
 io.on('connection', function(socket) {
   socket.on('sendEmailAddress', function(msg) {
     console.log(msg, socket.id);
 
-if (reg.test(msg)){
+    if (reg.test(msg)) {
 
-    var email = new Email();
-    email.address = msg;
+      var email = new Email();
+      email.address = msg;
 
-     Email.findOne({'address':msg}, 'address').then(email=>{
-      console.log('here it is', email);
-      return email!==null;
-     }).then(exists=>{
-      console.log(exists,'exists??')
-      if (exists){
-        console.log('its there already!')
-        io.emit('emailExtant', `emailExtant`)
-      } else  {
-         email.save(function(err) {
-      if (err)
-        console.log(err);
-      io.emit('loggedToDB', `email added ${msg}`);
+      Email.findOne({
+        'address': msg
+      }, 'address').then(email => {
+        console.log('here it is', email);
+        return email !== null;
+      }).then(exists => {
+        console.log(exists, 'exists??')
+        if (exists) {
+          console.log('its there already!')
+          io.emit('emailExtant', `emailExtant`)
+        } else {
+          email.save(function(err) {
+            if (err)
+              console.log(err);
+            var storyInfo=[]
 
-       });
+            request("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty", function(error, response, body) {
+              var topStories = JSON.parse(response.body).slice(0, 5);
 
-      }
-     })
-   } else {
-     io.emit('invalidEmail');
-   }
-   
+              topStories.forEach(storyId => {
+                request(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`, function(error, res, body) {
+                  var body=JSON.parse(res.body);
+                  storyInfo.push([body.by,body.score,body.title,body.url]);
+                  if (storyInfo.length===5){
+                  io.emit('loggedToDB', storyInfo);
+                }
+                })
+              })
+            });
+          });
+        }
+      })
+    } else {
+      io.emit('invalidEmail');
+    }
+
   });
 });
 
