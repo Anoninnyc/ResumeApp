21a0887c8fb41892fa648ce4b14f582dc5debfd7
Krishan Arya <krishanmarya1@gmail.com>
2016-09-22 23:52:11 +1000

    noComment

--- client/app.js
+++ client/app.js
@@ -14,7 +14,7 @@ myApp.directive('navBar', function() {
       replace: 'true',
       template: `<nav class="navbar navbar-default">
       <div class="container-fluid"><div class="navbar-header">
-      <a class="navbar-brand">Krishan M Aryas Resume</a>
+      <a class="navbar-brand">Krishan M Aryasss Resume</a>
     </div>
     <ul class="nav navbar-nav">
       <li><a href="/">Resume</a></li>
