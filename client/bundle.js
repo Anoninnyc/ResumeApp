8bb278fdb849eba13228a8407956fcb09508de1c
Krishan Arya <krishanmarya1@gmail.com>
2016-09-23 04:17:01 +1000

    cont_dynamic_rendering

--- client/bundle.js
+++ client/bundle.js
@@ -70,7 +70,7 @@
 	  $scope.repeatables = ["Send something, please!"];
 	  $scope.check = 'Angular is registered';
 
-	  dummyService.emailAction($scope, false, null, [],true);
+	  dummyService.emailAction($scope, false, null, []);
 
 	  $scope.sendEmailAddress = (address, name, company)=> {
 	      socket.emit('sendEmailAddress', {address, name, company});
@@ -79,7 +79,7 @@
 	    socket.once('loggedToDB', msg=> {
 	      console.log('reccccc')
 	        const addOn= msg.companyInfo? ` It looks like ${msg.companyInfo[1]} from ${msg.companyInfo[0]} has also registered interest!`: "";
-	        dummyService.emailAction($scope, false, `<h3>Email has been added, Congrats!${addOn}</h3>`, msg.storyInfo);
+	        dummyService.emailAction($scope, false, `<h3>Email has been added, Congrats!${addOn}</h3>`, msg.storyInfo, true);
 	         $scope.$apply();
 	    })
 
@@ -116,7 +116,7 @@
 	myApp.service('dummyService', function(){
 	  this.emailAction= (scope, loading , error, stories, clear)=>{
 	    [scope.loading, scope.error, scope.stories]=[loading, error, stories];
-	    if (clear===undefined){
+	    if (clear){
 	    	console.log('should clear');
 	    	$("#emailAddress").val("");
 	    	$("#emailName,#emailCompany").css({display:"none"});
--- client/source/services/services.js
+++ client/source/services/services.js
@@ -2,7 +2,8 @@ myApp.service('dummyService', function(){
   this.emailAction= (scope, loading , error, stories, clear)=>{
     [scope.loading, scope.error, scope.stories]=[loading, error, stories];
     if (clear){
-    	console.log('should clear');
+    	console.log(clear)
+    	
     	$("#emailAddress").val("");
     	$("#emailName,#emailCompany").css({display:"none"});
      }
