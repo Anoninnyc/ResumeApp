bf8e672ea2309468a2f20bacbcbb9b53c0a90e83
Krishan Arya <krishanmarya1@gmail.com>
2016-09-23 04:12:51 +1000

    cont_dynamic_rendering

--- client/source/services/services.js
+++ client/source/services/services.js
@@ -1,7 +1,7 @@
 myApp.service('dummyService', function(){
   this.emailAction= (scope, loading , error, stories, clear)=>{
     [scope.loading, scope.error, scope.stories]=[loading, error, stories];
-    if (!clear){
+    if (clear===undefined){
     	console.log('should clear');
     	$("#emailAddress").val("");
     	$("#emailName,#emailCompany").css({display:"none"});
