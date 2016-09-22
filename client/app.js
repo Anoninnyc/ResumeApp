8c5d252eea343bdc168685fde8bb378cab2906aa
Krishan Arya <krishanmarya1@gmail.com>
2016-09-22 22:45:30 +1000

    Fix typo

--- client/app.js
+++ client/app.js
@@ -4,5 +4,5 @@ var myApp = angular.module('myApp', ['ngRoute', 'ngSanitize'])
 // Directives
 
 require('./source/controllers/mainController.js');
-require('./source/services/mainController.js');
+require('./source/services/services.js');
 require('./source/config.js');
