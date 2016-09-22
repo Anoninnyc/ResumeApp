1f7934ea8b5072a4911c8a8e5cd05b75b9eb9fc7
Krishan Arya <krishanmarya1@gmail.com>
2016-09-23 05:23:29 +1000

    cont_dynamic_rendering

--- client/source/controllers/mainController.js
+++ client/source/controllers/mainController.js
@@ -26,29 +26,29 @@ myApp.controller('myCtrl', function($scope,dummyService) {
       console.log('reccccc')
         const addOn= msg.companyInfo? ` It looks like ${msg.companyInfo[1]} from ${msg.companyInfo[0]} has also registered interest!`: "";
         dummyService.emailAction($scope, false, `<h3>Email has been added, Congrats!${addOn}</h3>`, msg.storyInfo, true);
-         //$scope.$apply();
+         $scope.$apply();
     })
 
     socket.once('emailExtant', msg=> {
         dummyService.emailAction($scope, false, "<h3>It looks like you've already sent your info</h3>", []);
-        //$scope.$apply();
+        $scope.$apply();
     });
 
     socket.once('invalidEmail', msg=> {
         dummyService.emailAction($scope, false, "<h3>Please enter a valid Email Address.</h3>", []);
-        //$scope.$apply();
+        $scope.$apply();
     });
 
 
     socket.once('invalidName', msg=> {
 
         dummyService.emailAction($scope, false, "<h3>Please enter a valid name.</h3>", []);
-        //$scope.$apply();
+        $scope.$apply();
     });
 
      socket.once('invalidCompany', msg=> {
          dummyService.emailAction($scope, false, "<h3>Please enter a valid company name</h3>", []);
-       // $scope.$apply();
+         $scope.$apply();
     });
 
   };
