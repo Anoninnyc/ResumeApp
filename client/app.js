3f50409cb3aebeb58f90bb809a91989abb1a5b06
Krishan Arya <krishanmarya1@gmail.com>
2016-09-22 23:56:05 +1000

    correct_Directive

--- client/app.js
+++ client/app.js
@@ -12,15 +12,6 @@ myApp.directive('navBar', function() {
   return {
       restrict: 'AE',
       replace: 'true',
-      template: `<nav class="navbar navbar-default">
-      <div class="container-fluid"><div class="navbar-header">
-      <a class="navbar-brand">Krishan M Aryasss Resume</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li><a href="/">Resume</a></li>
-      <li><a href="/interested">Register Interest</a></li>
-    </ul>
-  </div>
-</nav>`
+      templateURL: './source/views/navBar.html'
   };
 });
--- client/bundle.js
+++ client/bundle.js
@@ -60,7 +60,7 @@
 	      replace: 'true',
 	      template: `<nav class="navbar navbar-default">
 	      <div class="container-fluid"><div class="navbar-header">
-	      <a class="navbar-brand">Krishan M Aryas Resume</a>
+	      <a class="navbar-brand">Krishan M Aryasss Resume</a>
 	    </div>
 	    <ul class="nav navbar-nav">
 	      <li><a href="/">Resume</a></li>
