abf5be56b2036959b85b1e84db66606e29fe50b3
Krishan Arya <krishanmarya1@gmail.com>
2016-09-22 22:36:48 +1000

    Add require statements:

--- client/app.js
+++ client/app.js
@@ -2,3 +2,7 @@ var myApp = angular.module('myApp', ['ngRoute', 'ngSanitize'])
 
 
 // Directives
+
+require('./source/controllers/mainController.js');
+require('./source/services/mainController.js');
+require('./source/config.js');
