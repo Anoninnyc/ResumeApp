917ec3d6246a50d35bd75047037559b12e461ace
Krishan Arya <krishanmarya1@gmail.com>
2016-09-23 23:49:15 +1000

    cont_dynamic_rendering_fixbug

--- client/bundle.js
+++ client/bundle.js
@@ -151,12 +151,12 @@
 	myApp.service('dummyService', function(){
 	  this.emailAction= (scope, loading , error, stories, clear)=>{
 	    [scope.loading, scope.error, scope.stories]=[loading, error, stories];
-	    
+
 	    if (clear){
 	    	console.log(clear)
 	    	
 	    	$("#emailAddress,#emailName,#emailCompany").val("");
-	    	$("#emailName,#emailCompany").css({display:"none"});
+	    	$("#emailName,#emailCompany").css({display:"none", padding:0, height:0});
 	     }
 	  }
 	})
