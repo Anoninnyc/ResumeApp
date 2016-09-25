bb48f4d5a2d4e2a20e7aff37844222a65d1ed996
Krishan Arya <krishanmarya1@gmail.com>
2016-09-25 19:49:06 +1000

    Animate

--- client/bundle.js
+++ client/bundle.js
@@ -188,9 +188,9 @@
 	  $rootScope.$on('$routeChangeSuccess', (e, current) => {
 	    $rootScope.count=3;
 
-	    setTimeout(function(){$rootScope.countdown(); $rootscope.$apply},500);
-	    setTimeout(function(){$rootScope.countdown(); $rootscope.$apply},1000);
-	    setTimeout(function(){$rootScope.countdown(); $rootscope.$apply},1500);
+	    setTimeout(function(){$rootScope.countdown(); $rootScope.$apply},500);
+	    setTimeout(function(){$rootScope.countdown(); $rootScope.$apply},1000);
+	    setTimeout(function(){$rootScope.countdown(); $rootScope.$apply},1500);
 
 
 	    moveIt!==undefined?clearInterval(moveIt):null;
