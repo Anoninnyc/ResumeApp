fedf554276cf2517d2a0cfec32e97dd8c4e356b3
Krishan Arya <krishanmarya1@gmail.com>
2016-10-15 14:58:34 +1100

    Add_service_fn

--- client/bundle.js
+++ client/bundle.js
@@ -275,6 +275,7 @@
 /***/ function(module, exports) {
 
 	myApp.service('dummyService', function(){
+		
 	  this.emailAction= (scope, loading , error, stories, clear)=>{
 	    [scope.loading, scope.error, scope.stories]=[loading, error, stories];
 
@@ -282,6 +283,24 @@
 			$("#emailAddress,#emailName,#emailCompany").val("");
 		 }
 	  }
+
+	  this.watchAction = (scope, watchVar, elem)=>{
+	     $("#action").css({opacity:1});
+	        scope[watchVar] = true;
+
+	        let el = $(`#${elem}`),
+	        curHeight = el.height(),
+	        autoHeight = el.css('height', 'auto').height();
+
+	        el.height(curHeight).css({
+	          padding: 0,
+	          display: "inline"
+	        }).animate({
+	          height: autoHeight,
+	          padding: 14
+	        }, 100);
+	  }
+
 	})
 
 /***/ },
--- client/source/services/services.js
+++ client/source/services/services.js
@@ -1,4 +1,5 @@
 myApp.service('dummyService', function(){
+	
   this.emailAction= (scope, loading , error, stories, clear)=>{
     [scope.loading, scope.error, scope.stories]=[loading, error, stories];
 
@@ -6,4 +7,22 @@ myApp.service('dummyService', function(){
 		$("#emailAddress,#emailName,#emailCompany").val("");
 	 }
   }
+
+  this.watchAction = (scope, watchVar, elem)=>{
+     $("#action").css({opacity:1});
+        scope[watchVar] = true;
+
+        let el = $(`#${elem}`),
+        curHeight = el.height(),
+        autoHeight = el.css('height', 'auto').height();
+
+        el.height(curHeight).css({
+          padding: 0,
+          display: "inline"
+        }).animate({
+          height: autoHeight,
+          padding: 14
+        }, 100);
+  }
+
 })
