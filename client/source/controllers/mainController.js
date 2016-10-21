8a6f4949db8df6438cfa707684857136d876f2e0
Krishan Arya <krishanmarya1@gmail.com>
2016-10-21 19:14:53 +1100

    Update_header

--- client/source/controllers/mainController.js
+++ client/source/controllers/mainController.js
@@ -61,10 +61,10 @@ myApp.controller('myCtrl', function($scope, dummyService) {
       socket.once('loggedToDB', msg => {
         const addOn = msg.companyInfo ? `\nIt looks like ${msg.companyInfo[1]} from ${msg.companyInfo[0]} has also registered interest!` : "";
         dummyService.emailAction($scope, false, `<h3>Email has been added- Thanks!${addOn}</h3>`, msg.storyInfo, true);
-        [$scope.comment, $scope.name, $scope.company, $scope.contactMessage] = ["", "", "", "     Wanna Send Again?      "];
-        $(".form-control").css({
-          width:"50%"
-        });
+        [$scope.comment, $scope.name, $scope.company, $scope.contactMessage] = ["", "", "", "Wanna Send Again?"];
+        // $(".form-control").css({
+        //   width:"70%"
+        // });
 
         // $("#sendInfo>#content").css({left:"20px"})
         $scope.$apply();
